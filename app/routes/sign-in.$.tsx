ec12b74419af2ea5ff05f2d35ea12894826eca54
evanfreeze <evanfreeze@gmail.com>
2024-01-27 10:48:02 -0600

    formatting

--- app/routes/sign-in.$.tsx
+++ app/routes/sign-in.$.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/remix"
 export default function SignInPage() {
   return (
     <div className="grid place-content-center min-h-[60vh]">
-        <SignIn />
+      <SignIn />
     </div>
   )
 }
